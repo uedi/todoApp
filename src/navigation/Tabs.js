90968a6941dbe5d72d09aabadf8d6a655c421787
uedi <uedi@users.noreply.github.com>
2023-01-23 13:43:34 +0200

    tab icons

--- src/navigation/Tabs.js
+++ src/navigation/Tabs.js
@@ -4,11 +4,25 @@ import Groups from '../screens/Groups'
 import Lists from '../screens/Lists'
 import Utils from '../screens/Utils'
 import More from '../screens/More'
+import { MaterialIcons } from '@expo/vector-icons'
 
 const TabsNav = createBottomTabNavigator()
 
-const tabsScreenOptions = () => ({
-    headerShown: false
+const tabsScreenOptions = ({ route }) => ({
+    headerShown: false,
+    tabBarIcon: ({ focused, size, color }) => {    
+        if(route.name === 'Home') {
+            return <MaterialIcons name='home' size={size} color={color} />
+        } else if(route.name === 'Groups') {
+            return <MaterialIcons name='groups' size={size} color={color} />
+        } else if(route.name === 'Lists') {
+            return <MaterialIcons name='list' size={size} color={color} />
+        } else if(route.name === 'Utils') {
+            return <MaterialIcons name='apps' size={size} color={color} />
+        } else if(route.name === 'More') {
+            return <MaterialIcons name='more-horiz' size={size} color={color} />
+        }
+    },
 })
 
 const Tabs = () => {
