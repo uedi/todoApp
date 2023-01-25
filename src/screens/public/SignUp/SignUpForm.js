48d69c62e0e357fb43c9e7f6fa3b7068befbfb6a
uedi <uedi@users.noreply.github.com>
2023-01-25 09:30:38 +0200

    signup form

---
+++ src/screens/public/SignUp/SignUpForm.js
@@ -0,0 +1,81 @@
+import { useState } from "react"
+import { View, StyleSheet, TextInput, Button, Text } from 'react-native'
+
+const SignUpForm = ({ signup }) => {
+    const [name, setName] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const valid = username !== '' && password !== '' && name !== ''
+
+    const handleSignup = () => {
+        if(valid) {
+            signup({
+                username: username,
+                password: password,
+                name: name
+            })
+        }
+    }
+
+    return (
+        <>
+            <Text style={styles.text}>Name</Text>
+            <TextInput
+                multiline={false}
+                style={styles.textInput}
+                textAlign='center'
+                value={name}
+                onChangeText={setName}
+            />
+            <Text style={styles.text}>Username</Text>
+            <TextInput
+                multiline={false}
+                style={styles.textInput}
+                textAlign='center'
+                value={username}
+                onChangeText={setUsername}
+            />
+            <Text style={styles.text}>Password</Text>
+            <TextInput
+                multiline={false}
+                style={styles.textInput}
+                textAlign='center'
+                value={password}
+                onChangeText={setPassword}
+            />
+            <View style={styles.button}>
+                <Button
+                    title='Signup'
+                    disabled={!valid}
+                    onPress={handleSignup}
+                />
+            </View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    text: {
+        alignSelf: 'center',
+        fontSize: 15,
+        marginTop: 15,
+        marginHorizontal: 15
+    },
+    textInput: {
+        alignSelf: 'stretch',
+        marginHorizontal: 15,
+        marginVertical: 5,
+        paddingHorizontal: 20,
+        backgroundColor: '#fff',
+        elevation: 0
+    },
+    button: {
+        alignSelf: 'center',
+        width: 200,
+        marginVertical: 15,
+        marginBottom: 50
+    }
+})
+
+export default SignUpForm
--- src/screens/public/SignUp/index.js
+++ src/screens/public/SignUp/index.js
@@ -1,17 +1,32 @@
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, KeyboardAvoidingView, Keyboard,
+    TouchableWithoutFeedback } from 'react-native'
+import SignUpForm from './SignUpForm'
 
 const SignUp = () => {
+
+    const handleSignup = data => {
+        console.log('signup', data)
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>SignUp</Text>
-        </View>
+        <KeyboardAvoidingView style={styles.container}>
+            <TouchableWithoutFeedback
+                onPress={Keyboard.dismiss}
+            >
+                <View style={styles.innerContainer}>
+                    <SignUpForm signup={handleSignup} />
+                </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1
+    },
+    innerContainer: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center'
     }
 })
