3d7c6af6ec52c795778b41cdf675a79642a32a86
uedi <uedi@users.noreply.github.com>
2023-01-29 18:28:04 +0200

    add username and name to account screen

--- src/screens/Account/index.js
+++ src/screens/Account/index.js
@@ -1,9 +1,9 @@
-import { View, StyleSheet, Button } from 'react-native'
-import { useDispatch } from 'react-redux'
+import { View, StyleSheet, Button, Text } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
 import { removeUser } from '../../reducers/userReducer'
 
 const Account = () => {
-
+    const user = useSelector(state => state.user)
     const dispatch = useDispatch()
 
     const handleLogout = () => {
@@ -12,6 +12,14 @@ const Account = () => {
 
     return (
         <View style={styles.container}>
+            <View style={styles.infoRow}>
+                <Text>Name</Text>
+                <Text style={styles.userInfo}>{user?.user?.name}</Text>
+            </View>
+            <View style={styles.infoRow}>
+                <Text>Username</Text>
+                <Text style={styles.userInfo}>{user?.user?.username}</Text>
+            </View>
             <Button
                 title='Logout'
                 onPress={handleLogout}
@@ -25,6 +33,16 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 25,
         alignItems: 'center'
+    },
+    infoRow: {
+        marginHorizontal: 30,
+        marginBottom: 30,
+        alignSelf: 'stretch',
+        flexDirection: 'row'
+    },
+    userInfo: {
+        flex: 1,
+        textAlign: 'right'
     }
 })
 
