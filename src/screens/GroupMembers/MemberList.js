2f6c25f332151122ba8de8535044751cb1852c3b
uedi <uedi@users.noreply.github.com>
2023-02-01 14:21:46 +0200

    group members list

---
+++ src/screens/GroupMembers/MemberList.js
@@ -0,0 +1,60 @@
+import { View, Text, StyleSheet, FlatList, TouchableNativeFeedback } from 'react-native'
+
+const ItemSeparator = () => (
+    <View style={{ height: 20 }} />
+)
+
+const MemberListItem = ({ member, clicked }) => {
+    return (
+        <View style={styles.container}>
+            <TouchableNativeFeedback
+                onPress={() => clicked(member)}
+            >
+                <View style={styles.innerContainer}>
+                    <Text style={styles.name}>{member.name}</Text>
+                </View>
+            </TouchableNativeFeedback>
+        </View>
+    )
+}
+
+const MemberList = ({ members, memberClicked }) => {
+    if(!members) {
+        return null
+    }
+
+    const renderItem = ({ item, index }) => (
+        <MemberListItem member={item} clicked={memberClicked} />
+    )
+
+    return (
+        <FlatList
+            data={members}
+            ItemSeparatorComponent={ItemSeparator}
+            renderItem={renderItem}
+            keyExtractor={(item, index) => item.contactId}
+            contentContainerStyle={{ paddingVertical: 15 }}
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        elevation: 1,
+        borderRadius: 20,
+        overflow: 'hidden'
+    },
+    innerContainer: {
+        flex: 1,
+        padding: 20
+    },
+    name: {
+        color: '#000',
+        fontSize: 17,
+        fontWeight: '700'
+    }
+})
+
+export default MemberList
--- src/screens/GroupMembers/index.js
+++ src/screens/GroupMembers/index.js
@@ -1,8 +1,27 @@
+import { useEffect, useState } from 'react'
 import { View, StyleSheet } from 'react-native'
+import MemberList from './MemberList'
+import { useSelector } from 'react-redux'
 
-const GroupMembers = () => {
+const GroupMembers = ({ route }) => {
+    const [members, setMembers] = useState()
+    const groups = useSelector(state => state.groups)
+    const id = route.params?.id
+
+    useEffect(() => {
+        if(members) {
+            const g = groups.find(g => g.id.toString() === id)
+            setMembers(g?.users ? g.users : [])
+        }
+    })
+
+    const memberClicked = member => {
+        console.log('clicked', member.name)
+    }
+
     return (
         <View style={styles.container}>
+            <MemberList members={members} memberClicked={memberClicked} />
         </View>
     )
 }
