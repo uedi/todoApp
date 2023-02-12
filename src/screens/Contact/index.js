26e687bd445475ef24fa789822976aab759ed874
uedi <uedi@users.noreply.github.com>
2023-02-12 16:08:19 +0200

    add qrcode to contact screen

--- src/screens/Contact/index.js
+++ src/screens/Contact/index.js
@@ -1,10 +1,12 @@
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, Dimensions } from 'react-native'
 import { useSelector } from 'react-redux'
+import QRCode from 'react-native-qrcode-svg'
 
 const Contact = ({ route }) => {
     const contacts = useSelector(state => state.contacts)
     const id = route.params?.id
     const contact = contacts && contacts.find(c => c.contactId.toString() === id)
+    const size = Dimensions.get('window').width / 2.5
 
     if (!contact) {
         return null
@@ -13,17 +15,24 @@ const Contact = ({ route }) => {
     return (
         <View style={styles.container}>
             <Text style={styles.name}>{contact.name}</Text>
+            { id &&
+                <QRCode
+                    value={id}
+                    size={size}
+                />
+            }
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        padding: 20,
+        alignItems: 'center'
     },
     name: {
-        marginTop: 10,
-        marginLeft: 30,
+        marginBottom: 30,
         fontSize: 17,
         fontWeight: 'bold'
     }
