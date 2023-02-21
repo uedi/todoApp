afc9b9a406440bb087b3ba1a93e0bac51448cda1
uedi <uedi@users.noreply.github.com>
2023-02-21 17:24:56 +0200

    add color picker to add contact

--- src/screens/AddContactManual/AddContactForm.js
+++ src/screens/AddContactManual/AddContactForm.js
@@ -1,17 +1,26 @@
 import { useState } from "react"
 import { View, StyleSheet, TextInput, Button, Text } from 'react-native'
+import DropDownPicker from 'react-native-dropdown-picker'
+import { backgroundColorsForSelect } from "../../utils/colors"
 
 const AddContactForm = ({ handleAdd }) => {
     const [username, setUsername] = useState('')
+    const [color, setColor] = useState()
+    const [colorPickerOpen, setColorPickerOpen] = useState(false)
 
     const valid = username !== ''
 
     const handleAddContact = () => {
         if(valid) {
             handleAdd({
-                username: username
+                username: username,
+                color: color ? color : null
             })
         }
+    }
+
+    const handleSetColor = valueFunc => {
+        setColor(valueFunc())
     }
 
     return (
@@ -23,6 +32,15 @@ const AddContactForm = ({ handleAdd }) => {
                 textAlign='center'
                 value={username}
                 onChangeText={setUsername}
+            />
+            <Text style={styles.text}>Color (optional)</Text>
+            <DropDownPicker
+                style={styles.picker(color)}
+                open={colorPickerOpen}
+                setOpen={setColorPickerOpen}
+                value={color}
+                setValue={handleSetColor}
+                items={backgroundColorsForSelect}
             />
             <View style={styles.button}>
                 <Button
@@ -55,7 +73,11 @@ const styles = StyleSheet.create({
         width: 200,
         marginVertical: 15,
         marginBottom: 50
-    }
+    },
+    picker: color => ({
+        alignSelf: 'stretch',
+        backgroundColor: color || ''
+    })
 })
 
 export default AddContactForm
