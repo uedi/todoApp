c116f75d1f594369ae5716d38953fb6ebb67ca32
uedi <uedi@users.noreply.github.com>
2023-02-22 12:33:04 +0200

    adjust public home

--- src/screens/public/GetStarted/index.js
+++ src/screens/public/GetStarted/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { View, StyleSheet, Button } from 'react-native'
+import { View, StyleSheet, Button, Text } from 'react-native'
 
 const GetStarted = ({ navigation }) => {
     return (
         <View style={styles.container}>
+            <Text
+                style={styles.topic}
+            >Todo App</Text>
             <Button
                 title='Signup'
                 onPress={() => navigation.navigate('SignUp')}
@@ -20,9 +23,16 @@ const GetStarted = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
+        alignItems: 'stretch',
         justifyContent: 'center',
-        paddingVertical: 75
+        paddingVertical: 50,
+        paddingHorizontal: 20
+    },
+    topic: {
+        alignSelf: 'center',
+        fontSize: 21,
+        marginBottom: 50,
+        fontWeight: '500'
     }
 })
 
